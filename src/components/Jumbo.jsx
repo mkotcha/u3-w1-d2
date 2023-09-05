3fee39be00cee50e804a0e6a6b10a31ec23fcdde
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-05 16:44:08 +0200

    new title

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -5,7 +5,7 @@ const Jumbo = props => {
     <Container>
       <div className="p-5 my-4 bg-body-tertiary rounded-3">
         <Container fluid className="py-5">
-          <h1 className="display-5 fw-bold">CBook Store</h1>
+          <h1 className="display-5 fw-bold">Fake Book Store</h1>
           <p className="col-md-8 fs-4">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam error blanditiis ea tempore saepe iusto
             itaque quos!
